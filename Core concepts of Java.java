ec01fc448631b306d41bfa029b5b671d222de7ec
ShaheerKhan <hussainkhan00023@gmail.com>
2020-01-07 11:16:48 +0500

    core concepts of java programming language

---
+++ Core concepts of Java.java
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		/* Variables (/*) ignore multi lines
+		how to declare Variables
+		syntex <datatype> <variable> = <input>; */
+		int a = 12;
+		System.out.println(a);
+		
+		// use doubble "" in java string 
+		String name = "SHAHeeRKhan";
+		System.out.println(name);
+		
+		// float datatpye require f in the end of variable
+		float b = 12.30f;
+		System.out.println(b);
+		
+		/*
+		Rules for constructing name of Variables:
+		1. Variable name can contains digits,underscore,dollar sign,latters
+		2. Shaheer and shaheer are different Variables
+		3. should not contains witespace
+		4. you can't use reserved keywords in java
+		*/
+		/*
+		Two types of java datatpye:
+		1.primitive
+		2.Non primitive OR reference data types
+		*/
+		
+		
+		
+	}
+}
